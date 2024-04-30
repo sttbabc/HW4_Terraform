51b4d0ad7b4967db4dd1a9a0200eda128e117d15
Babcock <babc1380@STTHOMAS.EDU>
2024-04-29 19:54:42 -0500

    added back outputs

--- main.tf
+++ main.tf
@@ -100,3 +100,16 @@ resource "aws_security_group" "rds_instance" {
     security_groups = [aws_security_group.web_servers.id]
   }
 }
+
+# Outputs
+output "ec2_instance1_public_ip" {
+  value = aws_instance.ec2_instance1.*.public_ip
+}
+
+output "ec2_instance2_public_ip" {
+  value = aws_instance.ec2_instance2.*.public_ip
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.example.endpoint
+}
